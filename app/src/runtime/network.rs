8364a2bff2d4d7ffd75976501c6a44d75b9e4728
Matt Wilson <matwils@microsoft.com>
2020-10-13 22:53:54 -0700

    Starting to build out the hosting side of things.

--- app/src/runtime/network.rs
+++ app/src/runtime/network.rs
@@ -1,40 +1,76 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::sync::mpsc;
+use std::thread;
 
-pub trait Connection {
-    fn send(&self, message: String) -> Result<(), Box<dyn std::error::Error>>;
-}
+// pub trait Connection {
+//     fn send(&self, message: String) -> Result<(), Box<dyn std::error::Error>>;
+// }
 
-pub struct Server {
+pub struct Connection {
     pub is_hosting: bool,
     listener: Option<TcpListener>,
     stream: Option<TcpStream>,
+    receive_queue: Vec<String>,
+    sender: Option<mpsc::Sender<String>>,
+    listening_thread: Option<thread::JoinHandle<()>>,
 }
 
-impl Server {
-    pub fn open_connection(address: String) -> Result<Server, Box<dyn std::error::Error>> {
-        let listener = TcpListener::bind(address)?;
-        
-        let connection = Server {
+impl Connection {
+    pub fn new() -> Connection {
+        Connection {
             is_hosting: false,
-            listener: Some(listener),
+            listener: None,
             stream: None,
-        };
-
-        Ok(connection)
+            receive_queue: Vec::new(),
+            sender: None,
+            listening_thread: None,
+        }
     }
-}
 
-pub struct Client {
-    pub is_connected: bool,
-}
+    pub fn host(&mut self, address: String) -> Result<(), Box<dyn std::error::Error>> {
+        let listener = TcpListener::bind(address).unwrap();
+        let (sender, receiver) = mpsc::channel();
 
-impl Client {
+        let thread = thread::spawn(move || {
+            let message = receiver.recv().unwrap();
+
+            // match message {
+            //     Message::NewJob(command) => {
+            //         println!("Worker got a job; executing.");
+
+            //         self.receive_queue.push(command.to_string());
+            //     }
+            //     Message::Terminate => {
+            //         println!("Worker was told to terminate.");
+            //         break;
+            //     }
+            // }
+        });
+
+        self.listening_thread = Some(thread);
+        self.sender = Some(sender);
+        
+        for stream in listener.incoming() {
+            let stream = stream?;
+            self.stream = Some(stream);
+            break;
+        }
+
+        Ok(())
+    }
+
     pub fn connect (&self, address: String) {
         // todo
     }
 
-    pub fn send(&self, content: String) {
-        // todo
+    pub fn handle_connection<F>(&self, f: F) where F: Fn(), F: Send {
     }
 }
+
+// type Job = Box<dyn ToString + Send + 'static>;
+
+// enum Message {
+//     NewJob(Job),
+//     Terminate,
+// }
